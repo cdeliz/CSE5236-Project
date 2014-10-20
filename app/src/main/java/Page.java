adb41bae882d3d4990c1cffb69a23e4c1303aa4b
cdeliz <chris.deliz@gmail.com>
2014-10-20 19:44:28 -0400

    First Real Commit
    Added first activity and got app running on device.

---
+++ app/src/main/java/Page.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Chris on 10/20/2014.
+ */
+public class Page {
+
+    //fields
+    public int width;
+    public int height;
+
+    //constructor
+    public Page(int width, int height) {
+
+        width = 1280;    //placeholder, final will be user-defined
+        height = 720;    //placeholder, final will be user-defined
+
+    }
+
+}
